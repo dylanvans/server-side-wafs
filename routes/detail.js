078577b509e750d9a71beb36b105a1329b7f1dd4
dylanvans <dylanvansprang@gmail.com>
2017-03-23 14:04:05 +0100

    Added browserify

--- routes/detail.js
+++ routes/detail.js
@@ -4,8 +4,8 @@ var router  = express.Router();
 router.get('/:id', function(req, res, next){
 	req.session.data.forEach(function(article) {
 		if(req.params.id == article.blocks.body[0].id) {
-			data = article
-			res.render('detail', data)
+			data = article;
+			res.render('detail', data);
 		}
 	});
 });
